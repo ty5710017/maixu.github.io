ccaf31420088e258dcebe72f9616dbea48e37228
ty5710017 <1031565575@qq.com>
2026-04-05 18:28:13 +0800

    Delete js directory

--- js/backtop.js
+++
@@ -1,7 +0,0 @@
-const backTop = document.getElementById('backTop');
-window.addEventListener('scroll', () => {
-    backTop.classList.toggle('show', window.scrollY > 300);
-});
-backTop.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
--- js/scroll.js
+++
@@ -1,4 +0,0 @@
-const header = document.querySelector('header');
-window.addEventListener('scroll', () => {
-    header.classList.toggle('scrolled', window.scrollY > 50);
-});
