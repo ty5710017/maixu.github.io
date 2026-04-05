07e3f89ced20934bd977cb4bb1793660fa4b3ecb
ty5710017 <1031565575@qq.com>
2026-04-05 15:49:18 +0800

    Remove copy button functionality for WeChat ID

--- js/copy.js
+++ js/copy.js
@@ -1,9 +1,1 @@
-const copyWxBtn = document.getElementById('copyWxBtn');
-copyWxBtn.addEventListener('click', () => {
-    navigator.clipboard.writeText('kj99990').then(() => {
-        copyWxBtn.textContent = '复制成功！';
-        setTimeout(() => {
-            copyWxBtn.textContent = '复制微信号';
-        }, 1500);
-    });
-});
+
